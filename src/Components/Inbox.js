2caa50e470ac5662537970e354e301ca63e221ac
unknown <tedokojikagami@gmail.com>
2021-12-16 16:14:43 +0700

    lazy inbox load because idk how to do messages yet

--- src/Components/Inbox.js
+++ src/Components/Inbox.js
@@ -1,10 +1,11 @@
+import inbox from './staticfiles/inox pic.png'
 import React from 'react'
-import NavBar from './NavBar'
 function Inbox() {
     const active = 'inbox'
     return (
         <div>
-           
+           <img style={{transform: 'scale(0.7)',marginTop: '-110px'}}
+            src={inbox}/>
         </div>
     )
 }
